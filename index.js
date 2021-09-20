24b6fd2696d9f7f04676c1adf59bba2c5b9b0bbd
Brendan Humphries <brendan.b.humphries@gmail.com>
2021-09-20 17:18:36 -0400

    commit tree a8c22f095c903ba8b506c09286e07bb033c6b9fa

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(block) {
+    if (block >= 42) {
+        return block - 42;
+    } else if (block < 42) {
+        return 42 - block;
+    }
+}
+function distanceFromHqInFeet(block) {
+    return distanceFromHqInBlocks(block) * 264;
+}
+function distanceTravelledInFeet(start, destination) {
+    if (start >= destination) {
+        return (start - destination) * 264;
+    } else if (start < destination) {
+        return (destination - start) * 264;
+    }
+}
+function calculatesFarePrice(start, destination) {
+    if (distanceTravelledInFeet(start, destination) > 2500) {
+        return 'cannot travel that far';
+    } else if (distanceTravelledInFeet(start, destination) > 2000) {
+        return 25;
+    } else if (distanceTravelledInFeet(start, destination) > 400) {
+        return (distanceTravelledInFeet(start, destination) - 400) * 0.02;
+    } else{
+        return 0
+    }
+}
